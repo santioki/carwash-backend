8f5b18bb5a941b3395db4923644347d3e9f2380a
santioki <mcskilled7@gmail.com>
2025-06-26 22:06:00 +0200

    Fix CORS to allow DELETE

--- server.js
+++ server.js
@@ -11,16 +11,20 @@ const adminRoute = require('./routes/admin');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+
 const corsOptions = {
-  origin: ["https://www.prowash.it.com"],
+  origin: "https://www.prowash.it.com",
   methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+  allowedHeaders: "Content-Type,Authorization",
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
+// Explicitly handle preflight
+app.options("*", cors(corsOptions));
+
+
 
 app.use(express.json());
 
