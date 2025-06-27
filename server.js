b59ed2871777fcb56e2db187bcfccf4b84ade354
santioki <mcskilled7@gmail.com>
2025-06-27 18:02:24 +0200

    Fix server error and stabilise deployment

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const corsOptions = {
 
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests
+app.options("/*", cors(corsOptions)); // Handle preflight requests
 
 // Middleware
 app.use(express.json());
